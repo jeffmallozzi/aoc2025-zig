4230b3b99a651f5963ea6fe7332808145cb6f132
Jeff Mallozzi <jeffmallozzi@gmail.com>
2026-03-07 14:33:54 -0500

    Day 4 Checkpoint

--- src/day04.zig
+++ src/day04.zig
@@ -24,6 +24,22 @@ const test_input =
     \\@.@.@@@.@.
 ;
 
+const Direction = enum {
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+const North: []const Direction = &.{Direction.Up};
+const NorthEast: []const Direction = &.{ Direction.Up, Direction.Right };
+const East: []const Direction = &.{Direction.Right};
+const SouthEast: []const Direction = &.{ Direction.Right, Direction.Down };
+const South: []const Direction = &.{Direction.Down};
+const SouthWest: []const Direction = &.{ Direction.Down, Direction.Left };
+const West: []const Direction = &.{Direction.Left};
+const NorthWes: []const Direction = &.{ Direction.Left, Direction.Up };
+
 const Location = struct { row: usize, col: usize };
 
 const Spot = struct {
@@ -31,20 +47,82 @@ const Spot = struct {
     countNeighors: usize = 0,
 };
 
-var wareHouse: std.AutoHashMap(Location, Spot) = .init(allocator);
+fn getNeighbor(location: Location, directions: []const Direction) !Location {
+    var row = location.row;
+    var col = location.col;
+
+    for (directions) |direction| {
+        switch (direction) {
+            Direction.Up => {
+                row = try std.math.sub(usize, row, 1);
+            },
+            Direction.Down => {
+                row += 1;
+            },
+            Direction.Left => {
+                col = try std.math.sub(usize, col, 1);
+            },
+            Direction.Right => {
+                col += 1;
+            },
+        }
+    }
+
+    return Location{ .row = row, .col = col };
+}
+
+fn getNeighbors(location: Location) []?Location {
+    var neighbors: [8]?Location = undefined;
+
+    neighbors[0] = getNeighbor(location, North) catch null;
+    neighbors[1] = getNeighbor(location, &[_]Direction{ Direction.Up, Direction.Right }) catch null;
+    neighbors[2] = getNeighbor(location, &[_]Direction{Direction.Right}) catch null;
+    neighbors[3] = getNeighbor(location, &[_]Direction{ Direction.Right, Direction.Down }) catch null;
+    neighbors[4] = getNeighbor(location, &[_]Direction{Direction.Down}) catch null;
+    neighbors[5] = getNeighbor(location, &[_]Direction{ Direction.Down, Direction.Left }) catch null;
+    neighbors[6] = getNeighbor(location, &[_]Direction{Direction.Left}) catch null;
+    neighbors[7] = getNeighbor(location, &[_]Direction{ Direction.Left, Direction.Up }) catch null;
+
+    return &neighbors;
+}
+
+fn addSpot(wareHouse: *std.AutoHashMap(Location, Spot), location: Location, spot: Spot) !void {
+    try wareHouse.put(location, spot);
+    if (spot.contents == '@') {
+        const neighbors = getNeighbors(location);
+
+        for (neighbors) |neighbor| {
+            if (neighbor != null) {
+                const val = wareHouse.getPtr(neighbor.?);
+                if (val) |loc| {
+                    if (loc.contents == '@') {
+                        loc.countNeighors += 1;
+                    }
+                }
+            }
+        }
+    }
+}
 
 fn solution1(gridInput: []const u8) !usize {
+    const trimedInput = std.mem.trim(u8, gridInput, "\n");
+    var warehouse: std.AutoHashMap(Location, Spot) = .init(allocator);
     var accessableRolls: usize = 0;
 
     var rowIndex: usize = 0;
-    var rows = splitScaler(u8, gridInput, '\n');
+    var rows = splitScaler(u8, trimedInput, '\n');
     while (rows.next()) |row| {
-        for (0..row.len) |colIndex| {
-            wareHouse.put(.{ .row = rowIndex, .col = colIndex }, .{
-                .contents = row[colIndex],
-            });
+        for (row, 0..) |item, colIndex| {
+            try addSpot(&warehouse, Location{ .row = rowIndex, .col = colIndex }, Spot{ .contents = item });
         }
         rowIndex += 1;
+    }
+
+    var warehouseSpots = warehouse.valueIterator();
+    while (warehouseSpots.next()) |spot| {
+        if ((spot.countNeighors < 4) & (spot.contents == '@')) {
+            accessableRolls += 1;
+        }
     }
 
     return accessableRolls;
