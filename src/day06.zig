018c044ec0bfc38083c1b40da1c62cae70f8aa80
Jeff Mallozzi <jeffmallozzi@gmail.com>
2026-06-24 16:14:19 -0400

    Day 6 Part 1

--- src/day06.zig
+++ src/day06.zig
@@ -19,19 +19,93 @@ const test_input =
     \\*   +   *   +
 ;
 
+const Operator = enum { Star, Plus };
+
 const Problem = struct {
     numbers: ArrayList(usize),
-    operator: u8,
+    operator: Operator,
+
+    fn solution(self: *const Problem) usize {
+        var reusult: usize = if (self.operator == Operator.Plus) 0 else 1;
+
+        for (self.numbers.items) |num| {
+            switch (self.operator) {
+                Operator.Plus => {
+                    reusult += num;
+                },
+                Operator.Star => {
+                    reusult *= num;
+                },
+            }
+        }
+
+        return reusult;
+    }
 };
 
-fn parse_input(input: []const u8) ArrayList(Problem) {
+fn parse_input(input: []const u8) !ArrayList(Problem) {
     // Need to split on varying amout of spaces ...
-    const lines = std.mem.split
+    var problems: ArrayList(Problem) = .empty;
+    var lines = std.mem.splitBackwardsScalar(u8, input, '\n');
+    const operators = lines.first();
+    var token_opps = std.mem.tokenizeAny(u8, operators, " ");
+
+    while (token_opps.next()) |operator| {
+        const opp = if (std.mem.eql(u8, operator, "*")) Operator.Star else Operator.Plus;
+        const prob = Problem{
+            .numbers = .empty,
+            .operator = opp,
+        };
+        try problems.append(allocator, prob);
+    }
+
+    while (lines.next()) |line| {
+        //std.debug.print("{s}\n", .{line});
+        var numbers = std.mem.tokenizeAny(u8, line, " ");
+        var i: usize = 0;
+        while (numbers.next()) |number| : (i += 1) {
+            //std.debug.print("{s} ", .{number});
+            const num = try parseInt(usize, number, 10);
+            try problems.items[i].numbers.append(allocator, num);
+        }
+    }
+
+    return problems;
+}
+
+pub fn sum_problems(problems: ArrayList(Problem)) usize {
+    var result: usize = 0;
+    for (problems.items) |problem| {
+        result += problem.solution();
+    }
+    return result;
 }
 
 pub fn get_solutions() !Solutions {
     var solutions = Solutions{};
-    solutions.sol1 = 0;
+    const problems = try parse_input(trimed_input);
+    solutions.sol1 = sum_problems(problems);
     solutions.sol2 = 0;
     return solutions;
+}
+
+test "parse_input" {
+    const problems = try parse_input(test_input);
+    try std.testing.expectEqual(4, problems.items.len);
+}
+
+test "problem" {
+    var problem = Problem{
+        .numbers = .empty,
+        .operator = Operator.Plus,
+    };
+    try problem.numbers.append(allocator, 4);
+    try problem.numbers.append(allocator, 5);
+    try problem.numbers.append(allocator, 6);
+    try std.testing.expectEqual(15, problem.solution());
+}
+
+test "Part_one" {
+    const problems = try parse_input(test_input);
+    try std.testing.expectEqual(4277556, sum_problems(problems));
 }
